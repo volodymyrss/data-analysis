2b0dbd256aa439073d1a92627dd392461ab8fbf4
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-09-19 16:46:08 +0000

    repack

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='data-analysis',
+    version='1.0',
+    py_modules=["analysisfactory", "backends", "bcolors", "caches", "dataanalysis", "hashtools", "importing", "printhook", "setup"],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
