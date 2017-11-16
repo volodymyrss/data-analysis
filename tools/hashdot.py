d7831fe3b86ea38bfd5a04d73f1b2911affc6d3e
Volodymyr SAVCHENKO <volodymyr.savchenko@unige.ch>
2017-11-16 17:50:14 +0100

    forgot plot_hashe in the tool

--- tools/hashdot.py
+++ tools/hashdot.py
@@ -1,6 +1,8 @@
 import argparse
 import ast
 
+from dataanalysis.displaygraph import plot_hashe
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('hashefiles', metavar='hashefiles', type=str, nargs='+',
