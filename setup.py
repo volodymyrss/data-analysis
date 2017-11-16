2b4653efd9e6d8a4993c58bb5e8b7172396c255f
Volodymyr SAVCHENKO <volodymyr.savchenko@unige.ch>
2017-11-16 16:19:41 +0100

    caches in installed packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='data-analysis',
     version='1.0',
-    packages=["dataanalysis"],
+    packages=["dataanalysis","dataanalysis.caches"],
     scripts=['tools/rundda.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
