c35a300c469842c215ebc1ef85f1f6d368e6a935
Volodymyr SAVCHENKO <volodymyr.savchenko@unige.ch>
2017-11-16 17:52:43 +0100

    hastdot installed as tool

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='data-analysis',
     version='1.0',
     packages=["dataanalysis","dataanalysis.caches"],
-    scripts=['tools/rundda.py'],
+    scripts=['tools/rundda.py','tools/hashdot.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
