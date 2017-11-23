e1fbd3ef69c8508fc3a10a1b6d82875556c38ac4
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-11-23 17:32:20 +0100

    factorizations noting and formatting

--- tests/test_graphspection.py
+++ tests/test_graphspection.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 
+import os
+
+
 def test_factorize():
 
     import dataanalysis.core as da
@@ -16,3 +19,21 @@ def test_factorize():
     c=CAnalysis().get()
     print(c._da_locally_complete)
 
+
+def test_factorize_note():
+
+    import dataanalysis.core as da
+    da.reset()
+
+    da.AnalysisFactory.note_factorization(dict(a=1,b=2,c=[1,2,{1:2}]))
+
+    class AAnalysis(da.DataAnalysis):
+        read_caches=[]
+        cached=True
+
+    A=AAnalysis().get()
+
+    fn=A._da_cached_path+"/factorizations.txt"
+    assert os.path.exists(fn)
+
+    print(">>>>\n"+open(fn).read()+"<<<<")
