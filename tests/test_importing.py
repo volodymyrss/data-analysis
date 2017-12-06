581f781e3c166e32744479f578bf2ccd83444048
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-12-06 16:57:19 +0100

    git maybe more suitable to travis

--- tests/test_importing.py
+++ tests/test_importing.py
@@ -13,12 +13,13 @@ def test_basic():
     assert not hasattr(m[0], '__dda_module_origin__')
 
 def test_git():
+    import os
     import dataanalysis.core as da
     import dataanalysis.importing as importing
 
     da.reset()
 
-    m=importing.load_by_name("git://ddosa",local_gitroot="/tmp/git")
+    m=importing.load_by_name("git://ddosa",local_gitroot=os.environ.get("TMPDIR","/tmp")+"/git/")
 
     assert hasattr(m[0], '__dda_module_global_name__')
     assert hasattr(m[0], '__dda_module_origin__')
