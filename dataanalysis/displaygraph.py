4034c3cdabad4e421e2a8b890ad4a8f022c94991
Volodymyr <vladimir.savchenko@gmail.com>
2018-03-07 14:02:13 +0100

    plot hashe modify

--- dataanalysis/displaygraph.py
+++ dataanalysis/displaygraph.py
@@ -2,9 +2,10 @@ import subprocess
 
 import pydot
 import time
+import hashtools
 
 
-def plot_hashe(hashe,pngfn,dotfn=None,show=True):
+def plot_hashe(hashe,pngfn,dotfn=None,show=True,assign_nuids=False,filter_nodes=None, assign_status=False):
     graph = pydot.Dot(graph_type='digraph', splines='ortho' )
 
     def process_hashe(hashe,graph,root=None):
@@ -16,12 +17,17 @@ def plot_hashe(hashe,pngfn,dotfn=None,show=True):
                 process_hashe(h,graph,root)
 
         if hashe[0]=="analysis":
-            graph.add_node(pydot.Node(repr(hashe[-1]).replace(":",""), style="filled", fillcolor="green", shape="box"))
+            node_label=repr(hashe[-1]).replace(":","")
+            if assign_nuids:
+                nuid=hashtools.shhash(hashe)[:8]
+                node_label+="_"+nuid
+
+            graph.add_node(pydot.Node(node_label, style="filled", fillcolor="green", shape="box"))
 
             if root is not None:
-                graph.add_edge(pydot.Edge(repr(hashe[-1]).replace(":",""), repr(root)))
+                graph.add_edge(pydot.Edge(node_label, repr(root)))
 
-            process_hashe(hashe[1],graph,hashe[-1])
+            process_hashe(hashe[1],graph,node_label)
 
         if isinstance(hashe,str):
             graph.add_node(pydot.Node(repr(hashe).replace(":",""), style="filled", fillcolor="green", shape="box"))
--- tools/hashdot.py
+++ tools/hashdot.py
@@ -9,10 +9,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('hashefiles', metavar='hashefiles', type=str, nargs='+',
                                help='an')
+    parser.add_argument('--outpng', metavar='hashefiles', type=str, help='an', default="graph.png")
+    parser.add_argument('--outdot', metavar='hashefiles', type=str, help='an', default="graph.dot")
+    parser.add_argument('--nuid', help='an', action='store_true', default=False)
 
     args = parser.parse_args()
 
     for hashefile in args.hashefiles:
         hashe=ast.literal_eval(open(hashefile).read())
-        pngfn="hashe.png"
-        plot_hashe(hashe,pngfn)
+        plot_hashe(hashe,args.outpng,dotfn=args.outdot,assign_nuids=args.nuid)
