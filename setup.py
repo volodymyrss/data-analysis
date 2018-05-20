d93f54e1e8e2c2e8f8727cd7e1084955a4fc5c65
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-05-20 23:13:57 +0200

    commandline

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     version='1.0',
     packages=["dataanalysis","dataanalysis.caches"],
     scripts=['tools/rundda.py','tools/hashdot.py'],
+    entry_points={
+            'console_scripts': [
+                    'dda-emerge = dataanalysis.emerge:main',
+                ]
+        },
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
