fe6565ab44f1ac1d5fb154f2ad48afbd09ac4677
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2019-03-13 20:11:08 +0100

    newway of script

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ setup(
     name='data-analysis',
     version='1.0',
     packages=["dataanalysis","dataanalysis.caches"],
-    scripts=['tools/rundda.py','tools/hashdot.py'],
+    entry_points={
+            'console_scripts': [
+                    'dda-emerge = dataanalysis.emerge:main',
+                    'dda-run = dataanalysis.rundda:main',
+                    'rundda.py = dataanalysis.rundda:main',
+                    'dda-hashdot = dataanalysis.hashdot:main',
+                    'hashdot.py = dataanalysis.rundda:main',
+                ]
+        },
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
