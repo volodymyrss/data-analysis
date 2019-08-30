07cbacc1665d9f721411f318ec4f90823a8e11cb
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2019-08-30 11:59:13 +0200

    scripts compatible with stable

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='data-analysis',
-    version='1.0',
+    version='1.0.0',
     packages=["dataanalysis","dataanalysis.caches"],
     entry_points={
             'console_scripts': [
-                    'dda-emerge = dataanalysis.emerge:main',
-                    'dda-run = dataanalysis.rundda:main',
-                    'rundda.py = dataanalysis.rundda:main',
-                    'dda-hashdot = dataanalysis.hashdot:main',
-                    'hashdot.py = dataanalysis.rundda:main',
+                    #'dda-emerge = dataanalysis.emerge:main',
+     #               'dda-run = dataanalysis.rundda:main',
+     #               'rundda.py = dataanalysis.rundda:main',
+     #               'dda-hashdot = dataanalysis.hashdot:main',
+     #               'hashdot.py = dataanalysis.rundda:main',
                 ]
         },
+    scripts=[
+                'tools/rundda.py',
+                'tools/hashdot.py',
+            ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
