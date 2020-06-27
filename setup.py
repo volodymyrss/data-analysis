56ac1978bde72d31fc28ee3ce9a131b0d479da08
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-27 19:07:20 +0200

    version

--- setup.py
+++ setup.py
@@ -2,22 +2,6 @@ from setuptools import setup
 
 setup(
     name='data-analysis',
-    version='1.0.4',
-    packages=["dataanalysis","dataanalysis.caches"],
-    entry_points={
-            'console_scripts': [
-                    'dda-emerge = dataanalysis.emerge:main',
-                    'dda-run = dataanalysis.rundda:main',
-                    'rundda.py = dataanalysis.rundda:main',
-                    'dda-hashdot = dataanalysis.hashdot:main',
-                    'hashdot.py = dataanalysis.rundda:main',
-                ]
-        },
-#    scripts=[
-#                'tools/rundda.py',
-#                'tools/dda-run',
-#                'tools/hashdot.py',
-#            ],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    version='1.1.0',
     long_description=open('README.md').read(),
 )
