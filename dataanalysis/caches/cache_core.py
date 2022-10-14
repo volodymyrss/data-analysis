6de537d1d8762878d782720d99bcb66f33252d32
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-14 21:33:07 +0000

    Adding tarfile member sanitization to extractall()

--- dataanalysis/caches/cache_core.py
+++ dataanalysis/caches/cache_core.py
@@ -583,7 +583,26 @@ class Cache(object):
             raise RuntimeError("temp restore dir exists before creating, impossible: {restored_dir}!")
 
         with tarfile.open(blob_fn, "r:gz") as tar:
-            tar.extractall(restored_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, restored_dir)
 
         return self.restore_from_dir(restored_dir+"/blob", hashe, obj, restore_config)
 
